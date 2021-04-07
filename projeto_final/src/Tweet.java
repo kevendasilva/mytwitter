3e5796c5625263fa42c9f8c8f385794d3236dcff
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2021-04-07 14:54:59 -0300

    Implementação da classe Tweet

--- projeto_final/src/Tweet.java
+++ projeto_final/src/Tweet.java
@@ -1,3 +1,22 @@
 public class Tweet {
-    
+
+  private String usuario;
+  private String mensagem;
+
+  public void setUsuario(String usuario){
+    this.usuario = usuario;
+  }
+
+  public String getUsuario(){
+    return usuario;
+  }
+
+  public void setMensagem(String mensagem){
+    this.mensagem = mensagem;
+  }
+
+  public String getMensagem(){
+    return mensagem;
+  }
+
 }
