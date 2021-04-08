04d80cb2f93d3e406b112a85599fb439d0767f6f
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2021-04-08 20:55:53 -0300

    Implementação da classe MyTwitter

--- projeto_final/src/ITwitter.java
+++ projeto_final/src/ITwitter.java
@@ -2,22 +2,22 @@ import java.util.Vector;
 
 interface ITwitter {
 
-  public void criarPerfil(Perfil usuario);
+  public void criarPerfil(Perfil usuario) throws PEException;
 
-  public void cancelarPerfil(String usuario);
+  public void cancelarPerfil(String usuario) throws PIException, PDException;
 
-  public void tweetar(String usuario, String mensagem);
+  public void tweetar(String usuario, String mensagem) throws PIException, PDException, MFPException;
 
-  public Vector<Tweet> timeline(String usuario); 
+  public Vector<Tweet> timeline(String usuario) throws PIException, PDException; 
 
-  public Vector<Tweet> tweets(String usuario);
+  public Vector<Tweet> tweets(String usuario) throws PIException, PDException;
 
-  public void seguir(String seguidor, String seguido);
+  public void seguir(String seguidor, String seguido) throws PIException, PDException, SIException;
 
-  public int numeroSeguidores(String usuario);
+  public int numeroSeguidores(String usuario) throws PIException, PDException;
 
-  public Vector<Perfil> seguidores(String usuario);
+  public Vector<Perfil> seguidores(String usuario) throws PIException, PDException;
 
-  public Vector<Perfil> seguidos(String usuario);
+  public Vector<Perfil> seguidos(String usuario) throws PIException, PDException;
 
 }
--- projeto_final/src/MyTwitter.java
+++ projeto_final/src/MyTwitter.java
@@ -1,3 +1,264 @@
-public class MyTwitter {
+import java.util.Vector;
+
+public class MyTwitter implements ITwitter{
+
+  // Atributos da classe MyTwitter
+
+  private Repositorio repositorio;
+
+
+  // Métodos da classe MyTwitter
+
+  public MyTwitter(Repositorio repositorio){
+    this.repositorio = repositorio;
+  }
+  
+  private Boolean isAtivoAndExiste(Perfil usuario) throws PIException {
+    Perfil perfil = null;
+
+    if (usuario != null) perfil = repositorio.buscar(usuario.getUsuario());
+
+    // O perfil existe?
+    if (perfil == null) {
+      // Não
+      throw new PIException();
+    } else {
+      // Sim
     
+      // Está ativo?
+      if (perfil.isAtivo()) {
+        // Sim
+        return true;
+      } else {
+        // Não
+        return false;
+      }
+    }
+  }
+
+  public void criarPerfil(Perfil usuario) throws PEException {
+    // Não é permitido cadastrar um perfil com o mesmo usuario
+    try {
+      // Tentando cadastrar
+      repositorio.cadastrar(usuario);
+    } catch (UJCException e) {
+      // Caso o usuario já tenha sido cadastrado
+      // A exceção PEException é levantada
+      throw new PEException();
+    }
+  }
+
+  public void cancelarPerfil(String usuario) throws PIException, PDException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+
+      try {
+        // Ativo recebe falso
+        perfil.setAtivo(false);
+
+        // Atualizando o repositório
+        repositorio.atualizar(perfil);
+      } catch (UNCException e) {
+        // Caso o usuário não esteja cadastrado no repositorio
+        // A exceção PIException será levantada. Não sendo necessário tratar a 
+        // exceção UNCException.
+      }
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+  public void tweetar(String usuario, String mensagem) throws PIException, PDException, MFPException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+      
+      // A mensagem possui entre 1 e 140 caracteres?
+      if (141 > mensagem.length() && mensagem.length() > 0) {
+        // Sim
+
+        // Criando o tweet
+        Tweet tweet = new Tweet();
+        // Adicionando a mensagem
+        tweet.setMensagem(mensagem);
+
+        // Adicionando o tweet ao perfil
+        perfil.addTweet(tweet);
+
+        // Atualizando a timeline dos seguidores do perfil
+        for (Perfil seguidor: perfil.getSeguidores()) {
+          if (seguidor.isAtivo()){
+            seguidor.addTweet(tweet);
+             // Atualizando o seguidor
+            try {
+              repositorio.atualizar(seguidor);
+            } catch (UNCException e) {}
+          }
+
+        }
+
+        // Atualizando o perfil do repositório
+        try {
+          repositorio.atualizar(perfil);
+        } catch (UNCException e) {}
+      } else {
+        // Não
+        throw new MFPException();
+      }
+    } else {
+      // Não
+      throw new PDException(); 
+    }
+  }
+
+  public Vector<Tweet> timeline(String usuario) throws PIException, PDException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+
+      // Retorne a timeline do usuario
+      return perfil.getTimeline();
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+  public Vector<Tweet> tweets(String usuario) throws PIException, PDException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    if(isAtivoAndExiste(perfil)) {
+      // Sim
+
+      // Tweets
+      Vector<Tweet> tweets = perfil.getTimeline();
+
+      // Percorrendo todos os tweets da timeline 
+      // e removendo aqueles que não pertecem ao usuario
+      for (Tweet tweet: tweets) {
+        if (!tweet.getUsuario().equals(perfil.getUsuario())) tweets.remove(tweet);
+      }
+
+      // Retorne os Tweets do usuario
+      return tweets;
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+  public void seguir(String seguidor, String seguido) throws PIException, PDException, SIException {
+    Perfil perfilSeguidor = repositorio.buscar(seguidor);
+    Perfil perfilSeguido = repositorio.buscar(seguido);
+
+    // O perfilSeguidor e/ou perfilSeguido existem e estão ativos?
+    if (isAtivoAndExiste(perfilSeguidor) && isAtivoAndExiste(perfilSeguido)) {
+      // Sim
+
+      // São iguais?
+      if (perfilSeguido.getUsuario().equals(perfilSeguidor.getUsuario())) {
+        // Sim
+        throw new SIException();
+      } else {
+        // Não
+        
+        // Adicionar os perfis em suas respectivas listas
+        perfilSeguidor.addSeguido(perfilSeguido);
+        perfilSeguido.addSeguidor(perfilSeguidor);
+
+        // Atualizando o repositorio
+        try {
+          repositorio.atualizar(perfilSeguido);
+          repositorio.atualizar(perfilSeguidor);
+        } catch (UNCException e) {}
+      }
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+  public int numeroSeguidores(String usuario) throws PIException, PDException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+
+      // Número de seguidores
+      int numSeguidores = 0;
+
+      Perfil seguidorRep;
+
+      // Verificando quais são os seguidores que estão ativos
+      for (Perfil seguidor: perfil.getSeguidores()) {
+        seguidorRep = repositorio.buscar(seguidor.getUsuario());
+        if (seguidorRep.isAtivo()) numSeguidores++;  
+      }
+
+      // Número de seguidores
+      return numSeguidores;
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+  public Vector<Perfil> seguidores(String usuario) throws PIException, PDException {
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    // O perfil existe?
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+
+      // Seguidores
+      Vector<Perfil> seguidores = perfil.getSeguidores();
+      Perfil seguidorRep;
+
+      for (Perfil seguidor: perfil.getSeguidores()) {
+        seguidorRep = repositorio.buscar(seguidor.getUsuario());
+        if (!seguidorRep.isAtivo()) seguidores.remove(seguidor);  
+      }
+      
+      return seguidores;
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
+
+
+  public Vector<Perfil> seguidos(String usuario) throws PIException, PDException{
+    Perfil perfil = repositorio.buscar(usuario);
+    // O perfil deve existir e estar ativo
+
+    // O perfil existe e está ativo?
+    if (isAtivoAndExiste(perfil)) {
+      // Sim
+
+      // Seguidos
+      Vector<Perfil> seguidos = perfil.getSeguidos();
+      Perfil seguidoRep;
+
+      for (Perfil seguido: perfil.getSeguidos()) {
+        seguidoRep = repositorio.buscar(seguido.getUsuario());
+        if (!seguidoRep.isAtivo()) seguidos.remove(seguido);  
+      }
+
+      return seguidos;
+    } else {
+      // Não
+      throw new PDException();
+    }
+  }
 }
